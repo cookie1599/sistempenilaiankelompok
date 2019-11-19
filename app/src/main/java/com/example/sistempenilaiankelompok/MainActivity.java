408ee19402127a3a3f4d3c47fd2f7bcdd312df05
cookie1599 <atrisaendya@gmail.com>
2019-11-19 11:54:54 +0700

    hae

--- app/src/main/java/com/example/sistempenilaiankelompok/MainActivity.java
+++ app/src/main/java/com/example/sistempenilaiankelompok/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout1_viewmahasiswa);
+        setContentView(R.layout.layout3_viewmahasiswa);
     }
 }
